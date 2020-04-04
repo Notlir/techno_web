2030e0967eaacf89baba0bc3f6612057dd7a01b7
Johann <johann.feschet@gmail.com>
2020-04-04 11:42:42 +0200

    first commit

---
+++ src/main/java/com/techno_web/techno_web/TechnoWebApplication.java
@@ -0,0 +1,13 @@
+package com.techno_web.techno_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techno_web/techno_web/TechnoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techno_web.techno_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
